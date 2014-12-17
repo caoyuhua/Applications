71191a7ba14e30e9bb505e8e53e533e295f60731
root <root@yuhua-virtual-machine.(none)>
2014-12-17 14:54:16 +0800

    【新增原因】sqlite3数据库编程demo程序注释

--- sqlite3/test/main.c
+++ sqlite3/test/main.c
@@ -18,8 +18,18 @@ per a[] = {
   
   
   
-// 这个函数可以用来打印出每行的信息    
-static int callback(void *NotUsed, int argc, char **argv, char **azColName){    
+/*
+int sqlite3_exec(
+sqlite3 * db,
+const char *sql,
+int (*callback)(void*,int,char **,char**),
+void *,
+char **errmsg
+);
+*/
+static int callback(void *NotUsed, int argc, char **argv, char **azColName){
+//第二个参数argc表示输出信息的总行数
+//第四个参数azColName表示该行信息列名称的指针，第三个参数argv表示该行信息列数据的指针 
     int i;    
     for(i=0; i<argc; i++){    
         printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");    
@@ -38,16 +48,16 @@ int main()
     int age;  
     int nCol;  
   
-    // 打开数据库连接     
+    //打开.db格式数据库，若不存在则新建数据库
     int nRet = sqlite3_open("e:\\db\\people.db",&pdb);    
     assert(SQLITE_OK == nRet);    
   
-    // 创建表    
+    //sql语句
     const char *sql = "CREATE TABLE IF NOT EXISTS person(name VARCHAR(128),"    
         "age INTEGER,"    
         "sex VARCHAR(7)"    
         ");";    
-    nRet = sqlite3_exec(pdb,sql,NULL,NULL,&zErrMsg);    
+    nRet = sqlite3_exec(pdb,sql,NULL,NULL,&zErrMsg);//执行sql语句在pdb数据库中创建一名称为person的表.    
     if (nRet != SQLITE_OK)    
     {    
         printf("%s\n",zErrMsg);    
@@ -63,33 +73,32 @@ int main()
         sqlite3_close(pdb);    
         return 1;    
     }  
-  
-    // 使用sqlite3_exec() 插入数据    
-    sql = "INSERT INTO person(name,age,sex) VALUES(\"Alice\",15,\"woman\");";    
+    //使用sql语句及sqlite3_exec来插入数据
+	sql = "INSERT INTO person(name,age,sex) VALUES(\"Alice\",15,\"woman\");";    
     nRet = sqlite3_exec(pdb,sql,NULL,NULL,&zErrMsg);    
-    assert(SQLITE_OK == nRet);  // 为了简单，这里不打印zErrMsg的值    
+    assert(SQLITE_OK == nRet);//若插入数据失败，此处只是assert断言并没打印出错误信息zErrMsg.
   
     sql = "INSERT INTO person(name,age,sex) VALUES(\"Bob\",18,\"man\");";    
     nRet = sqlite3_exec(pdb,sql,NULL,NULL,&zErrMsg);    
-    assert(SQLITE_OK == nRet);  // 为了简单，这里不打印zErrMsg的值    
+    assert(SQLITE_OK == nRet);
   
     sql = "INSERT INTO person(name,age,sex) VALUES(\"Charli\",11,\"man\");";    
     nRet = sqlite3_exec(pdb,sql,NULL,NULL,&zErrMsg);    
-    assert(SQLITE_OK == nRet);  // 为了简单，这里不打印zErrMsg的值    
+    assert(SQLITE_OK == nRet);
   
-    // 使用sqlite3_exec() 查询数据    
-    printf("=====query by sqlite3_exec()=====\n");  
+    //使用sql语句及sqlite3_exec来查询数据
+	printf("=====query by sqlite3_exec()=====\n");  
     sql = "SELECT name,age,sex FROM person;";    
     nRet = sqlite3_exec(pdb,sql,callback,NULL,&zErrMsg);    
     assert(SQLITE_OK == nRet);    
   
-    // 使用sqlite3_prepare_v2(), sqlite3_bind_...() 插入数据  
+// 以下为插入数据的另一种方式：使用sqlite3_prepare_v2、sqlite3_bind_text、sqlite3_bind_int、sqlite3_step等接口函数.
     sql = "INSERT INTO person(name,age,sex) VALUES(?,?,?);";  
     nRet = sqlite3_prepare_v2(pdb,sql,strlen(sql),&pstmt,&pzTail);  
     assert(SQLITE_OK == nRet);    
     int i;  
     for (i=0;i<sizeof(a)/sizeof(per);i++)  
-    {  
+    {//将结构体数组a中的所有数值依次插入数据库  
         nCol = 1;  
         sqlite3_bind_text(pstmt,nCol++,a[i].name,strlen(a[i].name),NULL);  
         sqlite3_bind_int(pstmt,nCol++,a[i].age);  
@@ -101,13 +110,20 @@ int main()
   
     sqlite3_finalize(pstmt);  
   
-    // 使用sqlite3_prepare_v2(), sqlite3_column_...() 查询数据  
-  
+//以下为查询数据的另一种方式：使用sqlite3_prepare_v2、sqlite3_step、sqlite3_column_text、sqlite3_column_int等接口函数.
+/*
+int sqlite3_prepare_v2(
+sqlite3 *db,
+const char *zSql,//sql语句(该语句是UTF-8编码的字符串)
+int nByte,//sql语句的最大字节数
+sqlite3_stmt **ppStmt,//OUT:SQL statement handle
+const char **pzTail
+)
+*/
     printf("====== query by sqlite3_prepare_v2()======\n");  
     sql = "SELECT name,age,sex FROM person;";  
     nRet = sqlite3_prepare_v2(pdb,sql,strlen(sql),&pstmt,&pzTail);  
     assert(SQLITE_OK == nRet);    
-  
     while(1)  
     {  
         nRet = sqlite3_step(pstmt);  
